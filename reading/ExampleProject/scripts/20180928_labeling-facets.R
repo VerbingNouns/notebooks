644a61ea212c0dcb27b2ffecc12f5c8828323e3c
VerbingNouns <lmackerman@gmail.com>
2019-05-30 15:09:01 +0100

    clean up files, not ready to publicize yet

--- reading/ExampleProject/scripts/20180928_labeling-facets.R
+++
@@ -1,8 +0,0 @@
-library(ggplot2)
-
-ggplot(ToothGrowth, aes(x=as.factor(dose), y=len, fill= as.factor(dose) )) +
-  geom_bar(stat = "identity") +
-  facet_wrap(~supp, scales="free_y", labeller = label_both)+
-  ylab ("deletion")+
-  theme_classic()+
-  theme(legend.position = "bottom")
